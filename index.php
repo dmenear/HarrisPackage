6717b3c9706b09b2cac5e8e400b6122c8e9dbb2c
dmenear <davidlmenear@gmail.com>
2018-01-21 01:02:20 -0500

    update home page title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Ray's Place</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="bs/css/bootstrap.min.css">
