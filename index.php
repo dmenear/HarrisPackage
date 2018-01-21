177e731ee21563cb81e064844f46be0d90663376
dmenear <davidlmenear@gmail.com>
2018-01-21 01:00:11 -0500

    add index page

---
+++ index.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bs/css/bootstrap.min.css">
+  <script src="jquery/jquery-3.3.1.min.js"></script>
+  <script src="bs/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h1>Ray's Place</h1>
+  <p>This is some text.</p>
+</div>
+
+</body>
+</html> 
